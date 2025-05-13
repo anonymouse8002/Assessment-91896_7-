693b6a944dd1f496f8ebe6c0db79700e350150b6
anonymouse8002 <25380@nayland.school.nz>
2025-05-14 11:47:05 +1200

    pygame testing.py

--- pygame testing.py
+++ pygame testing.py
@@ -1,3 +1,13 @@
 import pygame
 pygame.init()
-print("a")
+
+screen = pygame.display.set_mode((1000, 1000))
+pygame.display.set_caption("prototype 1")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
